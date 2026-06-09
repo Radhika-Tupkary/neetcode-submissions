b7b959e65c574e582e14fa0ff60aea4165b0f5a6
Radhika Tupkary <radhe2111@gmail.com>
2026-06-09 09:17:07 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,30 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack1 = []    # original order
+        self.stack2 = []    # elements in sorted order
+
+    def push(self, val: int) -> None:
+        self.stack1.append(val)
+
+        if self.stack2:
+            if self.stack2[-1] >= val:
+                self.stack2.append(val)
+        else:
+            self.stack2.append(val)
+
+    def pop(self) -> None:
+        if self.stack1:
+            if self.stack2:
+                if self.stack1[-1] == self.stack2[-1]:
+                    self.stack2.pop()
+            self.stack1.pop()
+
+    def top(self) -> int:
+        if self.stack1:
+            return self.stack1[-1]
+
+    def getMin(self) -> int:
+        if self.stack2:
+            return self.stack2[-1]
+        
