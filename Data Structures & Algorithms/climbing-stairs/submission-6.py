523256f285a4d82c6f3fa212e607bd90ddc587fb
Radhika Tupkary <radhe2111@gmail.com>
2026-06-10 09:56:19 -0700

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,33 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         cache = {}
+
+#         def numOfWays(i):
+#             if i == n:
+#                 return 1
+
+#             if i > n:
+#                 return 0
+
+#             if i in cache:
+#                 return cache[i]
+
+#             cache[i] = numOfWays(i + 1) + numOfWays(i + 2)
+
+#             return cache[i]
+
+#         return numOfWays(0)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        target, targetMinusOne = 1, 1
+        i = n-2
+        while i >= 0:
+            temp = targetMinusOne
+            targetMinusOne = targetMinusOne + target
+            target = temp
+            i = i-1
+
+        return targetMinusOne
+
+
