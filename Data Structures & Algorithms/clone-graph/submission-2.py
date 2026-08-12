1150d662c603e2d81ee84a1f0b1c926705cfae8a
Radhika Tupkary <radhe2111@gmail.com>
2026-08-11 23:19:50 -0700

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        old_to_new = {node: Node(node.val)}  # dictionary with 1st original node and its clone
+        q = deque([node])
+
+        while q:
+            cur = q.popleft()
+
+            for nei in cur.neighbors:
+                if nei not in old_to_new:
+                    # create neighbor's clone first, before appending it as new clone's neighbor
+                    old_to_new[nei] = Node(nei.val) 
+                    q.append(nei)
+                    
+                # append neighbor's clone to new nodes neighbor's list
+                old_to_new[cur].neighbors.append(old_to_new[nei])  
+
+        return old_to_new[node]
