8086a3dd5e47e88b3c8c99f95bdaad4e0116a944
Radhika Tupkary <radhe2111@gmail.com>
2026-08-13 09:47:43 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        cache = {}
+
+        def rob(i):
+            if i >= n:
+                return 0
+
+            if i == n-1:
+                return nums[i]
+
+            if i in cache:
+                return cache[i]
+
+            cache[i] = max(nums[i] + rob(i+2), rob(i+1))
+            return cache[i]
+
+        return rob(0)
